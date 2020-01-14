4f426370e874b140e7c92a48ebee3e890c74218f
NeddiePeng <576970513@qq.com>
2020-01-14 23:02:22 +0800

    <feat>(article):增加帖子列表

--- src/components/article/Article.vue
+++ src/components/article/Article.vue
@@ -11,38 +11,50 @@
                 <el-input v-model="input" placeholder="请输入内容"></el-input>
             </div>
             <div class="item submit">
-                <el-button type="primary"  icon="el-icon-search">主要按钮</el-button>
-                <el-button type="success"  icon="el-icon-search">主要按钮</el-button>
+                <el-button type="primary"  icon="el-icon-search">搜索</el-button>
+                <el-button type="success"  icon="el-icon-circle-plus-outline">创建帖子</el-button>
             </div>
         </div>
         <div class="content">
-            <el-table
-                    :data="tableData"
-                    style="width: 100%"
-                    :stripe="true"
-                    :border="true"
-                    :highlight-current-row="true">
-                <el-table-column fixed prop="id" label="ID"></el-table-column>
-                <el-table-column prop="name" label="姓名"></el-table-column>
-                <el-table-column prop="province" label="省份"></el-table-column>
-                <el-table-column prop="city" label="市区"></el-table-column>
-                <el-table-column prop="address" label="地址"></el-table-column>
-                <el-table-column prop="zip" label="邮编"></el-table-column>
+            <el-table :data="article_data" style="width: 100%" :stripe="true" :border="true" :highlight-current-row="true">
+                <el-table-column fixed prop="id" label="ID" width="60"></el-table-column>
+                <el-table-column prop="title" label="标题"></el-table-column>
+                <el-table-column prop="tags" label="标签"></el-table-column>
+                <el-table-column label="封面图">
+                    <template slot-scope="scope">
+                        <el-image lazy v-if="scope.row.type == 'images'" style="width: 100px; height: 50px" :src="scope.row.images.url" fit="cover"></el-image>
+                        <el-image lazy v-if="scope.row.type == 'video'" style="width: 100px; height: 50px" :src="scope.row.video.cover" fit="cover"></el-image>
+                    </template>
+                </el-table-column>
+                <el-table-column prop="type" label="帖子类型"></el-table-column>
+                <el-table-column prop="user_info.nickname" label="发布用户"></el-table-column>
+                <el-table-column label="状态" width="100">
+                    <template slot-scope="scope">
+                        <el-link type="success" v-if="scope.row.status == 1 && scope.row.is_delete == 2">正常</el-link>
+                        <el-link type="warning" v-if="scope.row.status == 0 && scope.row.is_delete == 2">待审核</el-link>
+                        <el-link type="danger" v-if="scope.row.status == 2 && scope.row.is_delete == 2">已拒绝</el-link>
+                        <el-link type="danger" v-if="scope.row.is_delete == 1">已删除</el-link>
+                    </template>
+                </el-table-column>
                 <el-table-column label="操作" width="300">
-                    <el-button size="mini" type="success" icon="el-icon-edit">Edit</el-button>
-                    <el-button size="mini" type="danger" icon="el-icon-delete" @click="open">Delete</el-button>
-                    <el-button size="mini" icon="el-icon-s-promotion">Detail</el-button>
+                    <template slot-scope="scope">
+                        <el-button size="mini" type="success" icon="el-icon-edit">编辑</el-button>
+                        <el-button size="mini" icon="el-icon-s-promotion" v-if="scope.row.status == 0 && scope.row.is_delete == 2" @click="handleReview(scope.row.id, 1, scope.$index)">通过</el-button>
+                        <el-button size="mini" icon="el-icon-s-promotion" v-if="scope.row.status == 0 && scope.row.is_delete == 2" @click="handleReview(scope.row.id, 2, scope.$index)">拒绝通过</el-button>
+                        <el-button size="mini" icon="el-icon-s-promotion" v-if="scope.row.status == 1 && scope.row.is_delete == 2" @click="delete_data(scope.row.id, 1, scope.$index)">不显示</el-button>
+                        <el-button size="mini" icon="el-icon-s-promotion" v-if="scope.row.is_delete == 1" @click="delete_data(scope.row.id, 2,scope.$index)">显示</el-button>
+                    </template>
                 </el-table-column>
             </el-table>
             <div class="pagination">
                 <el-pagination
                         @size-change="handleSizeChange"
                         @current-change="handleCurrentChange"
-                        :current-page="currentPage4"
-                        :page-sizes="[100, 200, 300, 400]"
-                        :page-size="100"
+                        :current-page="currentPage"
+                        :page-sizes="[15, 20, 30, 50]"
+                        :page-size="15"
                         layout="total, sizes, prev, pager, next, jumper"
-                        :total="400">
+                        :total="article_count">
                 </el-pagination>
             </div>
         </div>
@@ -50,227 +62,16 @@
 </template>
 
 <script>
-    import {list} from '../../request/blog/article'
+    import {list, updateStatus, delete_article} from '../../request/blog/article'
     export default {
         name: "Article",
         data() {
             return {
                 article_data:[],
                 article_count: 0,
-                currentPage4:4,
+                currentPage: 1,
                 input: '',
-                tableHeight: window.innerHeight - 240,
-                tableData: [{
-                    date: '2016-05-03',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-02',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-04',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-08',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-06',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }]
+                tableHeight: window.innerHeight - 240
             }
         },
         mounted() {
@@ -279,22 +80,33 @@
             });
         },
         methods: {
+            handleReview(id, status, index) {
+                updateStatus(this, {
+                    article_id: id,
+                    status: status
+                }, index);
+            },
             handleSizeChange(val) {
-                console.log(`每页 ${val} 条`);
+                list(this, {
+                    page: this.currentPage,
+                    limit: val
+                });
             },
             handleCurrentChange(val) {
-                console.log(`当前页: ${val}`);
+                list(this, {
+                    page: val
+                });
             },
-            open() {
+            delete_data(id, status, index) {
                 this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
                     confirmButtonText: '确定',
                     cancelButtonText: '取消',
                     type: 'warning'
                 }).then(() => {
-                    this.$message({
-                        type: 'success',
-                        message: '删除成功!'
-                    });
+                    delete_article(this, {
+                        article_id: id,
+                        status: status
+                    }, index);
                 }).catch(() => {
                     this.$message({
                         type: 'info',
--- src/request/blog/article.js
+++ src/request/blog/article.js
@@ -25,6 +25,50 @@ function list(context, params = {}) {
     });
 }
 
+
+/**
+ * 修改帖子状态
+ * @type {string}
+ */
+const $update_status_api = '/article/status';
+
+function updateStatus(context, params = {}, index) {
+    context.$post($update_status_api, params).then((response) => {
+        if(response.code !== 200) {
+            context.$message({
+                showClose: true,
+                message: response.message,
+                type: 'error',
+                offset: 100,
+                center: true
+            });
+        }
+        context.article_data[index].status = params.status;
+    });
+}
+
+/**
+ * 删除
+ * @type {string}
+ */
+const $del_api = '/article/delete';
+
+
+function delete_article(context, params = {}, index) {
+    context.$post($del_api, params).then((response) => {
+        if(response.code !== 200) {
+            context.$message({
+                showClose: true,
+                message: response.message,
+                type: 'error',
+                offset: 100,
+                center: true
+            });
+        }
+        context.article_data[index].is_delete = params.status;
+    });
+}
+
 export {
-    list
+    list, updateStatus, delete_article
 }
